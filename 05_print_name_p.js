ea80767fcb9ce0323c7d339463c39a254380dbec
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-03 22:14:39 +0530

    Create 05_print_name_p.js

---
+++ 05_print_name_p.js
@@ -0,0 +1,10 @@
+// Declare variables
+let name = "Abhishek";  
+let age = 20;  
+
+// Print values
+console.log("My name is " + name);
+console.log("My age is " + age);
+
+console.log(`My name is ${name}`);
+console.log(`My age is ${age}`);
