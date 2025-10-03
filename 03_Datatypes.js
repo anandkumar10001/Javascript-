f4a067c0ce16cdc361ee5b08f272eb820d90afff
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-03 21:22:25 +0530

    Update 03_Datatypes.js

--- 03_Datatypes.js
+++ 03_Datatypes.js
@@ -1,8 +1,30 @@
 //Primitive
 //String
+let name = "Abhishek";
+let greeting = 'Hello World';
+let template = `My age is ${age}`;   // template literal
+
 //Number
+let age = 25;          // integer
+let price = 99.99;     // floating point
+let infinityValue = Infinity;
+let notANumber = NaN;
+
 //BigInt
+let bigNumber = 123456789012345678901234567890n;
+
 //Boolean
+let isStudent = true;
+let isLoggedIn = false;
+
 //Undefined
+let x;
+console.log(x);  // undefined
+
 //Null
+let data = null;
+
 //Symbol
+let id = Symbol("id");
+let anotherId = Symbol("id");
+console.log(id === anotherId); // false (unique)
