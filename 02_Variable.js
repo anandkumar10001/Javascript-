fd0f4c3f9c646885f84fde88ae43bc718d2c2ed0
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-03 21:12:44 +0530

    Update 02_Variable.js

--- 02_Variable.js
+++ 02_Variable.js
@@ -1,28 +1,16 @@
 //Var
-//Function-scoped
-//Redeclaration
-//Reassignment
-//Hoisting
 var x = 10;
 var x = 20;  // Redeclaration allowed
 x = 30;      // Update allowed
 console.log(x); // 30
 
 //Let
-//Block-scoped
-//Redeclaration not allowed
-//Reassignment
-//Hoisting (but TDZ*)
 let y = 10;
 // let y = 20; Not allowed in same scope
 y = 30;       // Update allowed
 console.log(y); // 30
 
 //Const
-//Block-scoped
-//Redeclaration not allowed
-//Reassignment not allowed
-//Hoisting (but TDZ*)
 const z = 10;
 // z = 20; Error - cannot reassign
 console.log(z); // 10
