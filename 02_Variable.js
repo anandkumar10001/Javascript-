c9aa7b9a3bba48aa26f6152b6d6f2aa2999e6dc2
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-03 20:57:51 +0530

    Update 02_Variable.js

--- 02_Variable.js
+++ 02_Variable.js
@@ -4,3 +4,13 @@ var x = 20;  // Redeclaration allowed
 x = 30;      // Update allowed
 console.log(x); // 30
 
+//Let
+let y = 10;
+// let y = 20; Not allowed in same scope
+y = 30;       // Update allowed
+console.log(y); // 30
+
+//Const
+const z = 10;
+// z = 20; Error - cannot reassign
+console.log(z); // 10
